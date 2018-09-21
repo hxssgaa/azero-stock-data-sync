a6f79944b10d80976f0ccfc8d50b01efe4093d21
wuwei.hx <wuwei.hx@alibaba-inc.com>
2018-09-21 11:21:54 +0800

    fix ib tick problem

--- ib/ib_sync_helper.py
+++ ib/ib_sync_helper.py
@@ -254,13 +254,6 @@ def _inner_start_tick_sync_helper(contracts):
                 query_time = _get_offset_trading_datetime(trading_days, query_time, 1)
                 logging.warning('Tick %s skipped' % contract.symbol)
                 continue
-            if hist_tick_data[2]:
-                hist_tick_data = list(map(lambda x: (int_2_date_for_tick(x.time),
-                                                     x.mask,
-                                                     x.size,
-                                                     x.price,
-                                                     x.exchange,
-                                                     x.specialConditions), hist_tick_data[2]))
             if hist_tick_data[1] == 'historical_ticks_last':
                 query_time = _get_offset_trading_datetime(
                     trading_days, '%s 20:00:00' % query_time.split()[0], 1)
@@ -269,10 +262,17 @@ def _inner_start_tick_sync_helper(contracts):
                 logging.error('Tick ' + str(hist_tick_data))
                 base_req_id += 1
                 continue
-
             if not hist_tick_data:
                 logging.warning('Tick %s break' % contract.symbol)
                 break
+
+            if hist_tick_data[2]:
+                hist_tick_data = list(map(lambda x: (int_2_date_for_tick(x.time),
+                                                     x.mask,
+                                                     x.size,
+                                                     x.price,
+                                                     x.exchange,
+                                                     x.specialConditions), hist_tick_data[2]))
 
             query_time = _get_offset_trading_datetime(
                 trading_days, hist_tick_data[-1][0], 1)
--- peek_db.py
+++ peek_db.py
@@ -5,7 +5,7 @@ from utils import get_config
 
 def peek_tick_for_symbol(db, symbol):
     # db['%s-tick' % symbol].delete_many({})
-    tick_data = list(db['%s-tick' % symbol].find().sort([('dt', DESCENDING)]).limit(100))
+    tick_data = list(db['%s' % symbol].find({'type': 32}).sort([('dt', DESCENDING)]).limit(10000))
     if tick_data:
         print(tick_data[0])
         for e in tick_data:
@@ -15,4 +15,4 @@ def peek_tick_for_symbol(db, symbol):
 if __name__ == '__main__':
     cfg = get_config('db')
     db = MongoClient(cfg['DbHost'], int(cfg['DbPort']))['azero-stock']
-    peek_tick_for_symbol(db, 'US.IQ')
+    peek_tick_for_symbol(db, 'US.BGNE')
