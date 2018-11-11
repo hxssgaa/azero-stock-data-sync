f4afc91718211363dd93be0f8ddb54ebae4f1203
wuwei.hx <wuwei.hx@alibaba-inc.com>
2018-11-11 10:44:03 +0800

    fix issue

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 git pull
-docker run -p 5000:5000 -v ~/azero-stock-data-sync:/azero-stock-data-sync azero-stock-data-sync
+docker run -p 5000:5000 -v $(pwd):/azero-stock-data-sync azero-stock-data-sync
